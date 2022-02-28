4483cc767570de26a526d52fb3a6b92428a86be3
sbx_user1051 <sbx_user1051@169.254.184.245>
2022-02-28 01:41:40 +0000

    Mon Feb 28 01:41:40 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,8 +10,11 @@ def checkout(skus):
     if not set(counts.keys()).issubset({'A', 'B', 'C', 'D'}):
         return -1
     counter = 0
+    counter += counts.get('E', 0)*40
+    counts['B'] = counts.get('B', 0) - min(counts,get('E', 0)//2, counts.get('B', 0))
+    counter += counts.get['B']//2*45 + counts.get['B']%2*30
     counter += counts.get('D', 0)*15
     counter += counts.get('C', 0)*20
-    counter += counts.get('B', 0)//2*45 + counts.get('B', 0)%2*30
-    counter += counts.get('A', 0)//3*130 + counts.get('A', 0)%3*50
+    counter += counts.get('A', 0)//5*200 + counts.get('A', 0)%5//3*130 + counts.get('A', 0)%5%3*50
     return counter
+
